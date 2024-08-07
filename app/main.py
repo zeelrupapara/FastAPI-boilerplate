490a1fe45c074bcbf17097d23ab15df6c2eec671
zeelrupapara <zeelrupapara@gmail.com>
2024-08-07 10:58:30 +0530

    fix: debug add in the create app function

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.core.modules import init_routers, make_middleware
 from app.core.logger import load_logger
 from dotenv import load_dotenv
 import logging
+import os
 
 
 def create_app() -> FastAPI:
@@ -15,12 +16,14 @@ def create_app() -> FastAPI:
     load_logger()
     logging.info("loaded environment variables")
 
+    # initialize app
     app_ = FastAPI(
         title="FastAPI starter kit",
         description="FastAPI starter kit that is needed for every fastapi project.",
         version="1.0.0",
         docs_url="/api/v1/docs",
         middleware=make_middleware(),
+        debug=os.getenv("DEBUG"),
     )
 
     # initialize routers
